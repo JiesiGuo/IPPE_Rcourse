857a052a0d2b42749050d30cef109f6234d09226
JiesiGuo <jiesiguo@gmail.com>
2015-05-11 11:14:35 +1000

    fix Readme

--- sjPlot_Introduction/sjPlot_Jiesi.R
+++ sjPlot_Introduction/sjPlot_Jiesi.R
@@ -1,5 +1,8 @@
+---
+title: "sjPlot Introduction"
+output: html_document
+---
 
-##Installation
 
 # load packages
 library(sjPlot)
@@ -8,18 +11,18 @@ library(sjmisc)
 pisa<-read_spss("PISA12-AUS-motivation.sav", 
                 enc="UTF-8",                # character encoding
                 autoAttachVarLabels = TRUE) # attach variable labels
+
 # overview your data using `view_spss` and `sjt.df`
 view_spss(pisa[,1:8],
           #  showFreq = TRUE, 
           #  showPerc = TRUE
 )
 
-
 # display data description like 'describe' function in 'psych' package
-
+sjt.df(pisa[,10:15])
 # print correlation 
-sjt.corr(pisa[,20:28], 
-         fadeNS = FALSE,  
+sjt.corr(pisa[,20:28],  
+         missingDeletion = "pairwise",
          pvaluesAsNumbers = TRUE,  # FALSE: significant levels are indicated by asterisks
          showPValues = TRUE       # show exact pvalues
          #triangle = "lower"      # print the values in the lower triangle of the table
@@ -27,19 +30,17 @@ sjt.corr(pisa[,20:28],
 )
 
 # print summaries of linear models using `sjt.lm`
-# model1: math behavior is predicted by math self-concept, interest value, intrinsic value and gender
+# model1: math behavior is predicted by math self-concept, intrinsic value, utility value and gender
 model1 <- lm(MATBEH ~ SCMAT + INTMAT + INSTMOT + Gender, data=pisa)
-# model2: math workethic is predicted by math self-concept, interest value, intrinsic value and gender
+# model2: math workethic is predicted by math self-concept,intrinsic value, utility value and gender
 model2 <- lm(MATWKETH ~ SCMAT + INTMAT + INSTMOT + Gender, data=pisa)
 
 sjt.lm(model1, model2)
-
-
 # add custom labels and convert to APA output
 sjt.lm(model1, model2,
        showHeaderStrings = TRUE,
        stringB = "Estimate",
-       showStdBeta = TRUE,
+       showStdBeta = FALSE,
 #       stringCI = "Conf. Int.",
        pvaluesAsNumbers = FALSE,
        showStdError = TRUE,
@@ -51,28 +52,20 @@ sjt.lm(model1, model2,
 #      CSS = list(css.topcontentborder = "+font-size: 0px;")  # remove intercept
 #      remove.estimates = 2  # remove the second coefficient after intercept
        )       
-                                  
-# another package [texteg](http://cran.r-project.org/web/packages/texreg/texreg.pdf) - extract output from a vast majority of models                                  
-# extract similar output using `texreg`
-install.packages("texreg")
-library(texreg)
-screenreg(list(
-  extract(model1, summaries = c("Observations", "CFI", "SRMR")),
-  extract(model2, summaries = c("Observations", "CFI", "SRMR"))),
-  single.row=TRUE)
+                                     
 
 #  plot coefficients of linear model using `sjp.lm`
 sjp.lm(model1, type = "std") # plot standardized coefficients
 # testing model assumption including (tests for outliers, multicollinearity, non-normality of residual and outliers, non-normlity of residual, homoscedasticity)
 sjp.lm(model1, type = "ma",
-       showOriginalModelOnly = TRUE) # FLASE: the following graphical tests will be both applied to the original and to the updated model
+       showOriginalModelOnly = FALSE) # FLASE: the following graphical tests will be both applied to the original and to the updated model
 # investigate each single predictor of the model
 sjp.lm(model1, type = "pred")         
 
-# plotting interaction using `sjp.int`, which accepts _lm_, _glm_ and _lme4_.[see more examples](http://www.strengejacke.de/sjPlot/sjp.int/)
-install.packages("effects") 
+# plotting interaction effect using `sjp.int`, which accepts _lm_, _glm_ and _lme4_.[see more examples](http://www.strengejacke.de/sjPlot/sjp.int/)
+#install.packages("effects") 
 #Plotting interaction betweenn self-concept and intrinsic value in preiding math workethic
-mod_inter <- lm(scale(MATWKETH) ~ scale(SCMAT)#scale(INSTMOT), data=pisa, x = TRUE)
+mod_inter <- lm(scale(MATWKETH) ~ scale(SCMAT)*scale(INSTMOT), data=pisa, x = TRUE)
 sjt.lm(mod_inter)
 library(effects)
 sjp.int(mod_inter, type = "eff",
@@ -81,11 +74,12 @@ sjp.int(mod_inter, type = "eff",
         moderatorValues = "meansd") #use the mean as well as one standard deviation above and below the mean value.
 
 # provide an base-graphic-example for simple-slopes plot 
-f1<- function(x)(.38)#x+(.34)#(+1)+(.07)#(+1)#x
-f2<- function(x)(.38)#x+(.34)#0+(.07)#(0)#x
-f3<- function(x)(.38)#x+(.34)#(-1)+(.07)#(-1)#x
+plot.new()
+f1<- function(x)(.38)*x+(.34)*(+1)+(.07)*(+1)*x
+f2<- function(x)(.38)*x+(.34)*0+(.07)*(0)*x
+f3<- function(x)(.38)*x+(.34)*(-1)+(.07)*(-1)*x
 
-plot(f1, from=-1, to=1, lty=1,lwd=3, add=TRUE, ylab="MATWKETH (SD)", xlab="SCMAT (SD)", xlim=c(-1,1), ylim=c(-1,1))
+plot(f1, from=-1, to=1, lty=1,lwd=3, ylab="MATWKETH (SD)", xlab="SCMAT (SD)", xlim=c(-1,1), ylim=c(-1,1))
 plot(f2, from=-1, to=1, lty=3,lwd=3, add=TRUE, ylab="MATWKETH (SD)", xlab="SCMAT (SD)", xlim=c(-1,1), ylim=c(-1,1))
 plot(f3, from=-1, to=1, lty=5,lwd=3, add=TRUE, ylab="MATWKETH (SD)", xlab="SCMAT (SD)", xlim=c(-1,1), ylim=c(-1,1))
 legend("topleft", lty=c(1,2,3),lwd=c(3,3,3),legend=c('INSTMOT = +1SD','INSTMOT = Mean','INSTMOT = -1SD'))
@@ -100,45 +94,39 @@ sjt.glm(model3,model4,
                                     "Intention to study hard in Math vs. language classes"))
 
 # plot probability curves of predictors using `sjp.glm`
-#The odds ratios for continous predictors are a bit more difficult to interprete than categorial variables. Thus, you can also plot probability curves of all predictors (covariates, coefficients)
+#The odds ratios for continous predictors are a bit more difficult to interpret than categorial variables. Thus, you can also plot probability curves of all predictors (covariates, coefficients)
 sjp.glm(model3,
         axisLabels.y = predlab,
         type = "prob",
         show.se = TRUE)
 # plot ramdon effects of linear mixed effects using `sjp.lmer`
 library(lme4)
-pisa1 <-pisa[as.numeric(pisa$SCHOOLID) < 21, ] # select the first 20 classes
+pisa1 <-pisa[as.numeric(pisa$SCHOOLID) < 21, ] # select the first 20 schools for easy presentation
 modelRandomInt <- lmer(MATWKETH ~ SCMAT + INTMAT + INSTMOT + (1|SCHOOLID) , data = pisa1)
 summary(modelRandomInt)
 
-# plot standardized fixed effects
+# View standardized fixed effects
 sjp.lmer(modelRandomInt, type = "fe.std")
-# plot random intercept across groups
+
+# view random intercept across groups
 sjp.lmer(modelRandomInt)
 # plot fixed effects depending on group levels
 sjp.lmer(modelRandomInt, type = "fe.ri",
         vars = "SCMAT") # only for predictior math self-concept
-
 # view ramdom effets of linear mixed effects using `sjp.lmer`
 modelRandomSlope <- lmer(MATWKETH ~ SCMAT + INTMAT + INSTMOT + (SCMAT + INTMAT + INSTMOT|SCHOOLID) , data = pisa1)
 summary(modelRandomSlope)
 #Lets compare a random intercept to a random slope model
 anova(modelRandomInt, modelRandomSlope)
-# plot standardized fixed effects
+# view standardized fixed effects
 sjp.lmer(modelRandomSlope, type = "fe.std")
 # View random effects across groups
 sjp.lmer(modelRandomSlope)
-
 # note: random effects can not be ploted in this version
 # Plotting random effects of generalized linear mixed effects models using [sjp.glmer](http://www.strengejacke.de/sjPlot/sjp.glmer/) (not cover here)
 # plot principal component analyses (PCA) using `sjp.pca`
 # we need to specify the items which are used in PCA
-# recveive first item of scale containing self-concept, intrinsic value and utility value
-start <- which(colnames(pisa)=="ST42Q02")
-# recveive last item of scale containing self-concept, intrinsic value and utility value
-end <- which(colnames(pisa)=="ST29Q08")
-sjt.pca(pisa[,c(start:end)], 
+sjt.pca(pisa[,c(6:17)], 
         numberOfFactors = 3,
         showVariance=TRUE)
-
 
